2441fb9624cbe16b1896b6c66cb738d27a097f1a
anil-kumar-automation <anil.singala@zensar.com>
2023-01-13 17:46:28 +0530

    bdd api automation code and supported maven commands

---
+++ src/test/java/Options/TestRunner.java
@@ -0,0 +1,14 @@
+package Options;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features",
+        plugin = "json:target/jsonReports/cucumber-report.json",
+        glue = {"stepDefinations"}/*,tags= {"@AddPlace"}*/)
+public class TestRunner {
+//tags= {"@DeletePlace"}  compile test verify
+}
